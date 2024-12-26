c0a4f8db3efe0f30a223a06aa62202dbcbada1a2
Thegeekpod <supriyo7dey@gmail.com>
2024-12-26 20:57:22 +0530

    xx

--- resources/views/admin/subscriptions/create.blade.php
+++ resources/views/admin/subscriptions/create.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.layout.main')
 
-@section('title', env('APP_NAME') . ' | Create Subscription')
+@section('title', env('APP_NAME').' | Create Subscription')
 
 @section('content')
 <div class="row justify-content-center">
-
-    <div class="col-lg-11">
+  
+        <div class="col-lg-11">
         <div class="card">
             <div class="card-title text-center">
                 <h4>Create Subscription</h4>
@@ -13,12 +13,21 @@
             <div class="card-body">
                 <form action="{{ route('subscriptions.store') }}" method="POST">
                     @csrf
+                    
+                        <input type="hidden" id="user_id" name="user_id" class="form-control" placeholder="Enter User ID" value="{{ $userId}}" required>
+                    
 
-                    <input type="hidden" id="user_id" name="user_id" class="form-control" placeholder="Enter User ID"
-                        value="{{ $userId}}" required>
-
-
-                        <div class="form-group mb-3">
+                    <div class="form-group mb-3">
+                        <label for="subscription_plan">Subscription Plan</label>
+                        <select id="subscription_plan" name="subscription_plan" class="form-control" required>
+                            <option value="">Select Status</option>
+                            <option value="Starter Plan - $50" @selected($subscriptionPlan == 'Starter Plan - $50')>Starter Plan - $50</option>
+                            <option value="Professional Plan - $100" @selected($subscriptionPlan == 'Professional Plan - $100')>Professional Plan - $100</option>
+                            <option value="Elite Plan - $300" @selected($subscriptionPlan == 'Elite Plan - $300')>Elite Plan - $300</option>
+                        </select>
+                        
+                    </div>
+                    <div class="form-group mb-3">
                         <div id="password-section">
                             <label for="password">Enter Password (use for sales person)</label>
 
@@ -100,14 +109,12 @@
                     <div id="zelle_fields" class="d-none">
                         <div class="form-group mb-3">
                             <label for="zell_email">Zelle Email</label>
-                            <input type="email" id="zell_email" name="zell_email" class="form-control"
-                                placeholder="Enter Zelle Email">
+                            <input type="email" id="zell_email" name="zell_email" class="form-control" placeholder="Enter Zelle Email">
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="zell_phone">Zelle Phone</label>
-                            <input type="text" id="zell_phone" name="zell_phone" class="form-control"
-                                placeholder="Enter Zelle Phone">
+                            <input type="text" id="zell_phone" name="zell_phone" class="form-control" placeholder="Enter Zelle Phone">
                         </div>
                     </div>
 
@@ -115,30 +122,27 @@
                     <div id="ach_fields" class="d-none">
                         <div class="form-group mb-3">
                             <label for="ach_bank_name">ACH Bank Name</label>
-                            <input type="text" id="ach_bank_name" name="ach_bank_name" class="form-control"
-                                placeholder="Enter Bank Name">
+                            <input type="text" id="ach_bank_name" name="ach_bank_name" class="form-control" placeholder="Enter Bank Name">
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="ach_type">ACH Type</label>
-                            <select id="ach_type" name="ach_type" class="form-control">
+                            <select id="ach_type" name="ach_type" class="form-control" >
                                 <option value="">Select Status</option>
                                 <option value="Checking">Checking</option>
-                                <option value="Savings">Savings</option>
+                                <option value="Savings" >Savings</option>
                             </select>
-
+                           
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="ach_routing_number">ACH Routing Number</label>
-                            <input type="text" id="ach_routing_number" name="ach_routing_number" class="form-control"
-                                placeholder="Enter Routing Number">
+                            <input type="text" id="ach_routing_number" name="ach_routing_number" class="form-control" placeholder="Enter Routing Number">
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="ach_account_number">ACH Account Number</label>
-                            <input type="number" id="ach_account_number" name="ach_account_number" class="form-control"
-                                placeholder="Enter Account Number">
+                            <input type="number" id="ach_account_number" name="ach_account_number" class="form-control" placeholder="Enter Account Number">
                         </div>
                     </div>
 
@@ -148,7 +152,7 @@
                 </form>
             </div>
         </div>
-
+       
     </div>
 </div>
 
@@ -178,7 +182,7 @@
     document.addEventListener('DOMContentLoaded', () => {
         // Select the input by class or ID
         const phoneInput = document.querySelector('#zell_phone');
-
+        
         if (phoneInput) {
             phoneInput.addEventListener('input', function () {
                 formatPhone(this);
