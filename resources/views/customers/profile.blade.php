fa76a2d98b0de1c8929a1f48480554498718d8db
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Pro.local>
2025-07-08 21:40:37 +0530

    test

--- resources/views/customers/profile.blade.php
+++ resources/views/customers/profile.blade.php
@@ -87,7 +87,7 @@
                     style="width: 150px; height: 28px; object-fit:cover;" /></a>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('customerProfile') }}">Profile1</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('customerProfile') }}">Profile</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('customer.logout') }}">Logout</a></li>
                 </ul>
             </div>
