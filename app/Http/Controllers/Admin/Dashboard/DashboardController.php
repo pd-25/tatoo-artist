dfdbd33605e7cbb7f329a5727d86d5c1919e086c
artic0909 <alfag327@gmail.com>
2025-08-14 23:55:44 +0530

    add provide price function11

--- app/Http/Controllers/Admin/Dashboard/DashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardController.php
@@ -972,7 +972,7 @@ class DashboardController extends Controller
         $quote->save();
 
         // send mail
-        Mail::send('admin.email.quotepriceprovide', [
+        Mail::send('Admin.email.quotepriceprovide', [
             'user_name'      => $quote->user->name,
             'size'           => $quote->size,
             'color'          => $quote->color,
