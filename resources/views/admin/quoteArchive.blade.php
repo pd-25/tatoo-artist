07d0d7faf597930cf91779771eef164d1f6ef9b0
artic0909 <alfag327@gmail.com>
2025-09-13 20:44:36 +0530

    running

--- resources/views/admin/quoteArchive.blade.php
+++ resources/views/admin/quoteArchive.blade.php
@@ -2,6 +2,13 @@
 @section('title', env('APP_NAME') . ' | Quote')
 @section('content')
     <style>
+        .modal {
+    z-index: 9999 !important;
+}
+.modal-backdrop {
+    z-index: 9998 !important;
+}
+
         .myClass {
             width: 500px;
             height: 500px;
@@ -109,13 +116,27 @@
                                                     onclick="Sendlink({{ $quote->user_id }},{{ $quote->artist_id }},{{ $quote->id }})">Send
                                                     Link</button> -->
                                             @elseif($quote->link_send_status == 1)
-                                                <button class="btn btn-sm btn-warning"
+                                                <!-- <button class="btn btn-sm btn-warning"
                                                     onclick="AgainSendlink({{ $quote->user_id }},{{ $quote->artist_id }},{{ $quote->id }})">Again
-                                                    Send Link</button>
+                                                    Send Link</button> -->
                                             @else
-                                                <a href="{{ $quote->pdf_path }}" class="btn btn-sm btn-success"
-                                                    target="_blank">View Link</a>
+                                                <!-- <a href="{{ $quote->pdf_path }}" class="btn btn-sm btn-success"
+                                                    target="_blank">View Link</a> -->
                                             @endif
+
+                                                <button class="btn btn-sm btn-primary viewQuoteDetails"
+        data-id="{{ $quote->id }}"
+        data-size="{{ $quote->size }}"
+        data-color="{{ $quote->color }}"
+        data-whtogttato="{{ $quote->when_get_tattooed }}"
+        data-budget="{{ $quote->budget }}"
+        data-availability="{{ $availability }}"
+        data-fbv="{{ $quote->front_back_view }}"
+        data-created="{{ $quote_created_at }}"
+        data-desc="{{ $escapedDescription }}"
+        data-price="{{ $quote->provide_price ?? '' }}">
+        View Quote Details
+    </button>
 
                                             <form method="POST"
                                             action="{{ route('quote.delete', encrypt($quote->id)) }}"
@@ -150,10 +171,84 @@
 </div>
 
 
+<!-- Quote view modal -->
+ <!-- View Quote Details Modal -->
+<div class="modal fade" id="viewQuoteModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content animated flipInY">
+            <div class="modal-header">
+                <h4 class="modal-title">Quote Details</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h6><i class="fa fa-clone"></i> Other Information</h6>
+                        <div class="table-responsive pt-2">
+                            <table class="table table-bordered">
+                                <tr><th>Size</th><td id="quo_size">Not Provided</td></tr>
+                                <tr><th>Color</th><td id="quo_color">Not Provided</td></tr>
+                                <tr><th>When looking to get tattooed?</th><td id="quo_whtogttato">Not Provided</td></tr>
+                                <tr><th>Budget</th><td id="quo_budget">Not Provided</td></tr>
+                                <tr><th>Availability</th><td id="quo_availability">Not Provided</td></tr>
+                                <tr><th>Front/Back View</th><td id="quo_front_back_view">Not Provided</td></tr>
+                                <tr><th>Quote Created On</th><td id="quo_created">Not Provided</td></tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-12">
+                        <h6><i class="fa fa-clone pr-1"></i>Description</h6>
+                        <div class="quote-desc" id="quote_description"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 
+
+@section('script')
+<script>
+    $(document).ready(function() {
+        $(document).on("click", ".viewQuoteDetails", function() {
+            let size = $(this).data('size');
+            let color = $(this).data('color');
+            let when_to_get_tatto = $(this).data('whtogttato');
+            let budget = $(this).data('budget');
+            let availability = $(this).data('availability');
+            let front_back_view = $(this).data('fbv');
+            let created = $(this).data('created');
+            let quote_description = $(this).data('desc');
+
+            // Fill modal data
+            $("#quo_size").text(size || "Not Provided");
+            $("#quo_color").text(color || "Not Provided");
+            $("#quo_whtogttato").text(when_to_get_tatto || "Not Provided");
+            $("#quo_budget").text(budget ? `$${budget}` : "Not Provided");
+            $("#quo_availability").text(availability || "Not Provided");
+            $("#quo_front_back_view").text(front_back_view || "Not Provided");
+            $("#quo_created").text(created || "Not Provided");
+            $("#quote_description").html(quote_description || "Not Provided");
+
+            // Show modal
+            $("#viewQuoteModal").modal('show');
+        });
+    });
+</script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+@endsection
 
 
 
 @endsection
+
+
 
 
