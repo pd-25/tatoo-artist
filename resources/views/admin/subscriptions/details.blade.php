932e41a33960967d11dcb221dde99110c4cbc37a
artic0909 <alfag327@gmail.com>
2025-09-15 20:57:19 +0530

    changes done

--- resources/views/admin/subscriptions/details.blade.php
+++ resources/views/admin/subscriptions/details.blade.php
@@ -24,7 +24,7 @@
                         </p>
                         
                         <p><strong>Joining Date:</strong> {{ $subscription->user->created_at->format('m/d/Y') }}</p>
-                        <p><strong>Joining By:</strong> {{ $sales->name }}</p>
+                        <p><strong>Joining By:</strong> {{ $sales->name ?? 'N/A' }}</p>
                         <p><strong>Subscription Date:</strong> {{ $subscription->subscription_date?->format('m/d/Y') ?? 'Use For Trial' }}</p>
                     </div>
                     <div class="col-md-6">
