0b3a63a4e4144c259da618beacba308ed3d7a59b
thegeekpod <supriyo7dey@gmail.com>
2025-10-15 20:11:42 +0530

    ..

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
 {
     $schedule->command('app:subscription-cron')
-        ->dailyAt('14:40') // Runs every day at 12 AM
+        ->dailyAt('14:43') // Runs every day at 12 AM
         ->before(function () {
             Log::info('Subscription cron job started at ' . now());
         })
